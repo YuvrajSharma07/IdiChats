13bcd582c2d5d006dd068d7ae58293c242763959
Yuv <billiondollarone07@gmail.com>
2017-06-27 13:25:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function settingsbtn() {
+	var btn = document.getElementById('settingsbtn');
+	var text = document.getElementById('navtext');
+}
+setTimeout(function () {
+    document.title = "IdiChats"
+}, 1000);
+$(".gwhitetheme").click = function() {
+	$(".header").addClass("gwhitetheme");
+}
