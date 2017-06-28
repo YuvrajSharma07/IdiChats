bfc541d21e3ea82a239ea41a557b7c27e4f278d5
Yuv <billiondollarone07@gmail.com>
2017-06-28 23:36:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,387 @@ function settingsbtn() {
 setTimeout(function () {
     document.title = "IdiChats"
 }, 1000);
-$(".gwhitetheme").click = function() {
+
+// bgcolor theme codes:
+
+document.getElementById('gwtheme').onclick = function() {
 	$(".header").addClass("gwhitetheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
 }
+document.getElementById('ytheme').onclick = function() {
+	$(".header").addClass("yellowtheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('otheme').onclick = function() {
+	$(".header").addClass("orangetheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('lbtheme').onclick = function() {
+	$(".header").addClass("lbluetheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('ltheme').onclick = function() {
+	$(".header").addClass("lavendertheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('dbtheme').onclick = function() {
+	$(".header").addClass("dbluetheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('gtheme').onclick = function() {
+	$(".header").addClass("greytheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('lgtheme').onclick = function() {
+	$(".header").addClass("lgreentheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("whitetheme");
+	$(".header").removeClass("redtheme");
+}
+document.getElementById('dgtheme').onclick = function() {
+	$(".header").addClass("dgreentheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("redtheme");
+	$(".header").removeClass("whitetheme");
+}
+document.getElementById('ptheme').onclick = function() {
+	$(".header").addClass("purpletheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("redtheme");
+	$(".header").removeClass("whitetheme");
+}
+document.getElementById('dtheme').onclick = function() {
+	$(".header").addClass("darktheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("redtheme");
+	$(".header").removeClass("whitetheme");
+}
+document.getElementById('wtheme').onclick = function() {
+	$(".header").addClass("whitetheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("redtheme");
+	$(".header").removeClass("yellowtheme");
+}
+document.getElementById('ttheme').onclick = function() {
+	$(".header").addClass("tealtheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("redtheme");
+	$(".header").removeClass("whitetheme");
+}
+document.getElementById('rtheme').onclick = function() {
+	$(".header").addClass("redtheme");
+	$(".header").removeClass("greytheme");
+	$(".header").removeClass("lgreentheme");
+	$(".header").removeClass("dgreentheme");
+	$(".header").removeClass("orangetheme");
+	$(".header").removeClass("gwhitetheme");
+	$(".header").removeClass("darktheme");
+	$(".header").removeClass("lavendertheme");
+	$(".header").removeClass("purpletheme");
+	$(".header").removeClass("yellowtheme");
+	$(".header").removeClass("dbluetheme");
+	$(".header").removeClass("lbluetheme");
+	$(".header").removeClass("tealtheme");
+	$(".header").removeClass("whitetheme");
+}
+
+// fontfamily codes:
+
+document.getElementById('psans').onclick = function() {
+	$('body').addClass("productsans");
+	// $(".productsans").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('osans').onclick = function() {
+	$('body').addClass("opensans");
+	// $(".opensans").addClass("fonttactive");
+	$('body').removeClass("productsans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('robo').onclick = function() {
+	$('body').addClass("roboto");
+	// $(".roboto").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("productsans");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('ubun').onclick = function() {
+	$('body').addClass("ubuntu");
+	// $(".ubuntu").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("productsans");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('mont').onclick = function() {
+	$('body').addClass("montserrat");
+	// $(".montserrat").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("productsans");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('quic').onclick = function() {
+	$('body').addClass("quicksand");
+	// $(".quicksand").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("productsans");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('jose').onclick = function() {
+	$('body').addClass("josefinsans");
+	// $(".josefinsans").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("productsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('ptsans').onclick = function() {
+	$('body').addClass("ptsans");
+	// $(".ptsans").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("productsans");
+	$('body').removeClass("sourcesans");
+}
+document.getElementById('ssans').onclick = function() {
+	$('body').addClass("sourcesans");
+	// $(".sourcesans").addClass("fonttactive");
+	$('body').removeClass("opensans");
+	$('body').removeClass("roboto");
+	$('body').removeClass("ubuntu");
+	$('body').removeClass("montserrat");
+	$('body').removeClass("quicksand");
+	$('body').removeClass("josefinsans");
+	$('body').removeClass("ptsans");
+	$('body').removeClass("productsans");
+}
+
+// color theme codes:
+
+document.getElementById('wcolor').onclick = function() {
+	$('div').addClass("whitecolor");
+	$('div').removeClass("gwhitecolor");
+	$('div').removeClass("greycolor");
+	$('div').removeClass("darkcolor");
+	$('div').removeClass("blackcolor");
+}
+document.getElementById('gwcolor').onclick = function() {
+	$('div').addClass("gwhitecolor");
+	$('div').removeClass("whitecolor");
+	$('div').removeClass("greycolor");
+	$('div').removeClass("darkcolor");
+	$('div').removeClass("blackcolor");
+}
+document.getElementById('gcolor').onclick = function() {
+	$('div').addClass("greycolor");
+	$('div').removeClass("gwhitecolor");
+	$('div').removeClass("whitecolor");
+	$('div').removeClass("darkcolor");
+	$('div').removeClass("blackcolor");
+}
+document.getElementById('dcolor').onclick = function() {
+	$('div').addClass("darkcolor");
+	$('div').removeClass("gwhitecolor");
+	$('div').removeClass("greycolor");
+	$('div').removeClass("whitecolor");
+	$('div').removeClass("blackcolor");
+}
+document.getElementById('bcolor').onclick = function() {
+	$('div').addClass("blackcolor");
+	$('div').removeClass("gwhitecolor");
+	$('div').removeClass("greycolor");
+	$('div').removeClass("darkcolor");
+	$('div').removeClass("whitecolor");
+}
+
+// fontsize codes:
+/*
+document.getElementById('probtn').onclick = function() {
+	var pxval = document.getElementById('pxval').value;
+	$('html').fontSize = pxval "px";
+}
+*/
